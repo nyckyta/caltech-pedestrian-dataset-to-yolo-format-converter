9e681a3aee4a2ee515111522f0a1750451dbae6e
Nikita <nykyta.leonov@gmail.com>
2020-05-08 01:56:47 +0300

    Change script API. Add requirments to specify directory with images, output directory and size of square image i.e. length of square side

--- squarify-images.py
+++ squarify-images.py
@@ -1,16 +1,30 @@
 import glob
-import Image
+from PIL import Image
+import sys
+import os
 
-frame_size = (640, 640)
-directory = 'images'
+default_frame_size = (640, 640)
 
-for frame in sorted(glob.glob(directory + '/*.png')):
-	# enlarge 640 x 480 frames up to frame_size 
-	# by filling the bottom area with whitespace
-	new_frame = Image.new('RGB', frame_size, 'white')
-	new_frame.paste(Image.open(frame), (0, 0))
+def squarify_img(dir_with_img, output_dir, square_side):
+	for frame in sorted(glob.glob(dir_with_img + '/*.png')):
+		new_frame = Image.new('RGB', default_frame_size, 'white')
+		new_frame.paste(Image.open(frame), (0, 0))
+		new_frame.thumbnail((square_side, square_side))
 
-	new_frame_path = frame.replace('.png', '_squared.png')
-	new_frame.save(new_frame_path, 'png')
+		#takes only image name , replace it from *.png to *_squared.png and then concatenate it with output directory
+		new_frame_path = output_dir + '/' +frame[-frame[::-1].find('/'):].replace('.png', '_squared.png') 
+		new_frame.save(new_frame_path, 'png')
 
 	print('saved ' + new_frame_path)
+
+if __name__ == '__main__':
+	if len(sys.argv) < 4:
+		raise("Need to specify <path_to_directory_of_images> <output_path> <square_length>")
+	dir_with_img = sys.argv[1]
+	out_dir = sys.argv[2]
+	square_side = int(sys.argv[3])
+
+	if not os.path.isdir(out_dir):
+		os.mkdir(out_dir)
+
+	squarify_img(dir_with_img, out_dir, square_side)
