2d858a924d3f1d92d912edb719102dfeb13247dd
Nikita <nykyta.leonov@gmail.com>
2020-05-07 19:45:44 +0300

    Add multithreading. Provides way to run conversion in multithread regime by passing additional CL paramerer with number of threads. Each thread unpacks archive and starts to convert .seq files to .jpg format, so it means that best perfomance appears when thread amount equal to archive amount (of cource if you have enough cores in your CPU).

--- generate-images.py
+++ generate-images.py
@@ -3,6 +3,7 @@
 
 import os
 import shutil
+import threading
 import sys
 import glob
 import cv2 as cv
@@ -14,8 +15,6 @@ def save_img(dname, fn, i, frame, out_dir):
 		os.path.basename(fn).split('.')[0], i), frame)
 
 def convert(dir, output_dir):
-	if not os.path.isdir(output_dir):
-		os.mkdir(output_dir)
 	for dname in sorted(glob.glob(dir)):
 		for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
 			cap = cv.VideoCapture(fn)
@@ -28,7 +27,7 @@ def convert(dir, output_dir):
 				i += 1
 			print(fn)
 
-def unpack_and_convert(entry, output_dir):
+def unpack_and_convert(entry, output_dir, semaphore):
 	full_path = entry.path
 	entry_name = entry.name
 	extraction_folder_name = entry_name[:-4]
@@ -38,21 +37,40 @@ def unpack_and_convert(entry, output_dir):
 
 	convert(extraction_folder_name, output_dir)
 	shutil.rmtree(extraction_folder_name)
+	if semaphore != None:
+		semaphore.release()
+
 
 def convert_data_from_tars_folder(dir_with_tars, output_dir):
 	with os.scandir(dir_with_tars) as entries:
 		for entry in entries:
-			unpack_and_convert(entry, output_dir)
+			unpack_and_convert(entry, output_dir, None)
 
+def convert_data_from_tars_folder_in_parallel(dir_with_tars, output_dir, semaphore):
+	with os.scandir(dir_with_tars) as entries:
+		for entry in entries:
+			semaphore.acquire()
+			threading.Thread(target=unpack_and_convert, args=(entry, output_dir, semaphore)).start()
 
 
 if __name__ == '__main__':
 	args = sys.argv
-	
+	job_counter = 0
+
 	if len(args) < 2:
 		raise Exception('Directory with CallTech archives should be passed')
-	if (len(args) < 3):
+	if len(args) < 3:
 		raise Exception('Output directory should be passed')
+	if len(args) > 3:
+		job_counter = int(args[3])
+		print('Job counter = ', job_counter)
 
-	convert_data_from_tars_folder(args[1], args[2])
+	if not os.path.isdir(args[2]):
+		os.mkdir(args[2])
+
+	if job_counter > 0:
+		semaphore = threading.Semaphore(job_counter)
+		convert_data_from_tars_folder_in_parallel(args[1], args[2], semaphore)
+	else:	
+		convert_data_from_tars_folder(args[1], args[2])
 
