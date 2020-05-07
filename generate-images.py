280ff72b7a2a0e54aa86c3d57d349c0090c6edb4
Nikita <nykyta.leonov@gmail.com>
2020-05-07 18:01:18 +0300

    Add output CL parameter

--- generate-images.py
+++ generate-images.py
@@ -2,22 +2,20 @@
 # adapted from https://github.com/mitmul/caltech-pedestrian-dataset-converter/blob/master/scripts/convert_seqs.py
 
 import os
+import shutil
 import sys
 import glob
 import cv2 as cv
 import tarfile as tar
 
-
-def save_img(dname, fn, i, frame):
+def save_img(dname, fn, i, frame, out_dir):
 	cv.imwrite('{}/{}_{}_{}.png'.format(
 		out_dir, os.path.basename(dname),
 		os.path.basename(fn).split('.')[0], i), frame)
 
-out_dir = 'images'
-if not os.path.exists(out_dir):
-	os.makedirs(out_dir)
-
-def convert(dir):
+def convert(dir, output_dir):
+	if not os.path.isdir(output_dir):
+		os.mkdir(output_dir)
 	for dname in sorted(glob.glob(dir)):
 		for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
 			cap = cv.VideoCapture(fn)
@@ -26,25 +24,35 @@ def convert(dir):
 				ret, frame = cap.read()
 				if not ret:
 					break
-				save_img(dname, fn, i, frame)
+				save_img(dname, fn, i, frame, output_dir)
 				i += 1
 			print(fn)
 
-def convert_data_from_tars_folder(dir_with_tars):
+def unpack_and_convert(entry, output_dir):
+	full_path = entry.path
+	entry_name = entry.name
+	extraction_folder_name = entry_name[:-4]
+
+	tarfile = tar.open(full_path)
+	tarfile.extractall()
+
+	convert(extraction_folder_name, output_dir)
+	shutil.rmtree(extraction_folder_name)
+
+def convert_data_from_tars_folder(dir_with_tars, output_dir):
 	with os.scandir(dir_with_tars) as entries:
 		for entry in entries:
-			full_path = entry.path
-			entry_name = entry.name
-			print('{} - current file'.format(full_path))
-			tarfile = tar.open(full_path)
-			print('.Seq files will be extracted into ./{}'.format(entry_name))
-			tarfile.extractall()
-			convert(entry_name.split('.')[0])
+			unpack_and_convert(entry, output_dir)
+
+
 
 if __name__ == '__main__':
 	args = sys.argv
 	
 	if len(args) < 2:
 		raise Exception('Directory with CallTech archives should be passed')
-	convert_data_from_tars_folder(args[1])
+	if (len(args) < 3):
+		raise Exception('Output directory should be passed')
+
+	convert_data_from_tars_folder(args[1], args[2])
 
